b92854a784c301b2396d6a97924e349f93030598
lc <34017828+lcccl@users.noreply.github.com>
2018-04-11 10:19:06 +0800

    增加分布式组件，包含锁和队列。

---
+++ base/src/main/java/base/framework/distributed/impl/redis/JedisExecutorProxy.java
@@ -0,0 +1,43 @@
+package base.framework.distributed.impl.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by cl on 2018/4/9.
+ * JedisExecutor动态代理
+ */
+public class JedisExecutorProxy implements InvocationHandler {
+
+    private JedisPool jedisPool;
+
+    private JedisExecutorProxy(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            return method.invoke(jedis, args);
+        } finally {
+            // 使用完后关闭连接
+            String methodName = method.getName();
+            if (!methodName.equals("close") && null != jedis) {
+                jedis.close();
+            }
+        }
+    }
+
+    public static JedisExecutor createProxy(JedisPool jedisPool) {
+        JedisExecutorProxy proxy = new JedisExecutorProxy(jedisPool);
+        return (JedisExecutor) Proxy.newProxyInstance(proxy.getClass().getClassLoader(),
+                new Class<?>[]{JedisExecutor.class}, proxy);
+    }
+
+}
